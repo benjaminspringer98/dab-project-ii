b71dcf5b70911c5f05f9aa5a3adafff7eb4f9f2b
Benjamin Springer <benni@MacBook-Air-von-Benjamin.local>
2023-11-18 18:42:22 +0200

    add home btn

--- qa-ui/src/layout/Header.svelte
+++ qa-ui/src/layout/Header.svelte
@@ -3,5 +3,6 @@
 </script>
 
 <nav class="p-4 mb-4 shadow">
+  <a href="/" class="text-2xl text-gray-700 font-serif m-5">Home</a>
   <span class="text-2xl text-gray-700 font-serif">Hello {$userUuid}!</span>
 </nav>
